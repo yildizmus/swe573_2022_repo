547f2a0c2f8c390a29621b7db1be4cc62ecc95f0
yildizmus <mustafayldz80@gmail.com>
2022-04-26 14:33:16 +0300

    Space model is created.

--- space/admin.py
+++ space/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from jmespath import search
+from .models import Space
 
-# Register your models here.
+class SpaceAdmin(admin.ModelAdmin):
+    list_display=('title', 'author', 'created_date')
+    search_fields=('title', 'content')
+    list_filter=['created_date']
+
+admin.site.register(Space, SpaceAdmin)
--- space/models.py
+++ space/models.py
@@ -1,3 +1,23 @@
+from enum import unique
+from pyexpat import model
 from django.db import models
+from autoslug import AutoSlugField
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Space(models.Model):
+    author=models.ForeignKey(User, on_delete=models.CASCADE, related_name="spaces")
+    title=models.CharField(max_length=50, blank=False, null=False)
+    content=RichTextField()
+    created_date=models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+
+    slug=AutoSlugField(populate_from='title', unique=True)
+
+    class Meta:
+        db_table='space' # This name is used for db_table name
+        verbose_name_plural="Spaces" #This name is used for Admin Panel dashboard
+        verbose_name="Space" #This name is used for Admin Panel messages
+    
+    def __str__(self):
+        return str(self.title)
+
