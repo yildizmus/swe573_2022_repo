f2ccc65e9dad439278e9cc745502ba8a15170856
yildizmus <mustafayldz80@gmail.com>
2022-05-17 15:27:18 +0300

    search button in the navbar is activated, it is possible to search in the sapce content and space title.

--- space/views/homepage.py
+++ space/views/homepage.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from space.models import SpaceModel
 from django.core.paginator import Paginator
+from django.db.models import Q
+
 
 
 def homepage(request): 
+    search=request.GET.get('search')
 
     allspaces=SpaceModel.objects.order_by('-id')
+    if search:
+        allspaces= allspaces.filter(
+            Q(title__icontains=search) |
+            Q(content__icontains=search)
+        ).distinct()
 
     page=request.GET.get('page')
     paginator=Paginator(allspaces,3)
