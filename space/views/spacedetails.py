ef5496eb077e15e4680c235624afa2bd13355bdd
yildizmus <mustafayldz80@gmail.com>
2022-05-20 22:42:58 +0300

    spacedetails.html template is updated, only space members can view/display the messaging contents.

--- space/views/spacedetails.py
+++ space/views/spacedetails.py
@@ -5,8 +5,7 @@ from space.models import SpaceModel, message
 #@login_required(login_url='/')
 def spacedetails(request, slug): 
     space=get_object_or_404(SpaceModel, slug=slug)
-    messages=space.messages.all()
-    
+    messages=space.messages.all()    
     return render(request,'pages/spacedetails.html', context={
         'space':space,
         'messages':messages,
