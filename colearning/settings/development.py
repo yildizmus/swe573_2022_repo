a1509b4e96285be37cc0a0ac77d1f9301d983200
yildizmus <mustafayldz80@gmail.com>
2022-11-07 18:00:38 +0300

    Initial settings is adjusted in order to be able to run the SWE 573 project.

--- colearning/settings/development.py
+++ colearning/settings/development.py
@@ -9,7 +9,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
